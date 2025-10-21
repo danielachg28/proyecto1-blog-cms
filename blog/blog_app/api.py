e2aaf116fe3c0cdc1a712f248aa2c0082eb6bae8
Tu Nombre <danielachaparro@z1.digital>
2025-10-21 10:09:00 +0200

    refactor(api): clean up and add comments

--- blog/blog_app/api.py
+++ blog/blog_app/api.py
@@ -13,10 +13,16 @@ from django.contrib.auth.models import User
 
 
 # ViewSet para Blog
-class BlogViewSet(viewsets.ModelViewSet):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class BlogViewSet(
+    viewsets.ModelViewSet
+):  # viewset que permite crear, leer, actualizar y eliminar modelos
+    queryset = (
+        Blog.objects.all()
+    )  # Indica que datos manejará el viewset (en este caso, todos los blogs)
+    serializer_class = BlogSerializer  # Indica el serializador que se usará para convertir los datos a JSON
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]  # Indica los permisos que se usarán para acceder a los datos
 
 
 # ViewSet para Post
@@ -27,8 +33,8 @@ class PostViewSet(viewsets.ModelViewSet):
 
     # Asociar el post al blog del usuario autenticado
     def perform_create(self, serializer):
-        blog = self.request.user.blog
-        serializer.save(blog=blog)
+        blog = self.request.user.blog  # Obtiene el blog del usuario autenticado
+        serializer.save(blog=blog)  # Guarda el post con el blog del usuario autenticado
 
 
 # ViewSet para Tag
@@ -40,5 +46,5 @@ class TagViewSet(viewsets.ModelViewSet):
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = [AllowAny]  # cualquiera puede registrarse
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]  # cualquiera puede registrarse
