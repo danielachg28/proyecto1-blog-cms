ddd8bf7eb29de0d8390651e73c234722b5954f0d
Tu Nombre <danielachaparro@z1.digital>
2025-10-23 10:15:12 +0200

    test(test_models): update tag function

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,13 +1,11 @@
 import pytest
 
-from tests.factories import BlogFactory, PostFactory, UserFactory
+from tests.factories import BlogFactory, PostFactory, TagFactory, UserFactory
 
 
 @pytest.mark.django_db
-def test_user_factory_creates_valid_user():
-    """
-    Verifica que la UserFactory crea un usuario válido y funcional.
-    """
+def test_user_factory_creates_valid_user():  # Verifica que la UserFactory crea un usuario válido y funcional.
+
     user = UserFactory()
     assert user.username is not None  # Se genera un nombre de usuario
     assert user.password != " "  # Tiene una contraseña cifrada
@@ -27,3 +25,15 @@ def test_post_is_linked_to_blog():  # Crea un post y verifica que esté asociado
 
     post = PostFactory()
     assert post.blog is not None  # El post debe tener un blog asociado
+
+
+@pytest.mark.django_db
+def test_tag_is_created_with_post():  # Verifica que un Tag se cree correctamente y que se asocie a un Post
+
+    post = PostFactory()  # Crea un post de prueba
+    tag = TagFactory(posts=[post])  # Crea un tag asociado a ese post
+    assert tag.posts.exists()  # Debe tener al menos un post asociado
+    linked_post = tag.posts.first()  # Obtiene el primer post asociado
+    assert tag in linked_post.tags.all()  # La relación debe funcionar en ambos sentidos
+    assert linked_post.blog is not None  # El post debe tener un blog asociado
+    assert " " not in tag.name  # El nombre del tag debe ser una sola palabra
