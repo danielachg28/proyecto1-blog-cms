5e327a4888acb5babfa2a1900ea230e54d3cba21
Tu Nombre <danielachaparro@z1.digital>
2025-11-06 19:50:53 +0100

    fix(dockerfile): update files

--- blog/start.sh
+++ blog/start.sh
@@ -5,10 +5,6 @@ set -e  # detiene el script si algún comando falla
 # Opcional: mostrar en logs el PORT que Railway asignó
 echo "Railway PORT: $PORT"
 
-# Instalar dependencias
-pip install --no-cache-dir --upgrade pip
-pip install --no-cache-dir -r requirements.txt
-
 # Aplicar migraciones automáticamente
 python blog/manage.py migrate --noinput
 
